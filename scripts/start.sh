272c61619a7dc23174df7130bf82bc52b57c5a44
Nigel Stephen D'Souza <nigelsd@gmail.com>
2016-12-15 01:15:15 +0000

    Ability to specify multiple instances

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,8 +2,25 @@
 
 . ~/bin/setup_node_env.sh
 
+
+# Default instance
+DEPLOYMENT="general"
+
+# extract options and their arguments into variables.
+while getopts d: FLAG; do
+	case $FLAG in
+		d)
+			DEPLOYMENT=$OPTARG
+			;;
+		\?) echo "Internal error! Unrecognized argument $FLAG" ; exit 1 ;;
+	esac
+done
+
+
+
 appname=${PWD##*/}
 export NODE_APP_INSTANCE="${appname}"
-export NODE_ENV="prod"
+
+export NODE_ENV="${DEPLOYMENT}"
 
 node index.js
