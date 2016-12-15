879f521fff0145aab787abd4bc5c8ffe8e90ff2b
Nigel Stephen D'Souza <nigelsd@gmail.com>
2016-12-15 01:12:43 +0000

    Specific configs for different instances

--- config/default-email-cleanup.js
+++ config/default-email-cleanup.js
@@ -9,6 +9,24 @@ module.exports = {
     scopes: ['https://mail.google.com']
   },
 
+  log: {
+    log4jsConfigs: {
+      appenders: [
+        {
+          type:       "file",
+          filename:   defer(function (cfg) { return cfg.log.logDir.concat("/" , cfg.appName , '-' , process.env.NODE_APP_INSTANCE , ".log" ) }),
+          category:   defer(function (cfg) { return cfg.appName }),
+          reloadSecs: 180,
+          maxLogSize: 512000
+        },
+        {
+          type: "console"
+        }
+      ],
+      replaceConsole: true
+    }
+  },
+
   mailboxes: {
     personal: {
       auth: {
